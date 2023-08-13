cd29f44853d2238b18d868213e0f8933dae117f2
Dhiraj <dkskp2005@gmail.com>
2023-08-13 21:51:51 +0530

    overrides

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
-import { EuiProvider } from '@elastic/eui'
-import React from 'react'
+import { EuiProvider, EuiThemeProvider } from "@elastic/eui";
+import React from "react";
 
-import '@elastic/eui/dist/eui_theme_light.css';
-import { Route, Routes } from 'react-router-dom';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
+import "@elastic/eui/dist/eui_theme_light.css";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
+  //override the defaults
+  const overrides = {
+    colors: {
+      LIGHT: { primary: "#0b5cff" },
+      DARK: { primary: "#0b5cff" },
+    },
+  };
+
   return (
     <EuiProvider>
-      <Routes>
-        <Route path='/login' element={<Login />}/>
-        <Route path='/' element={<Dashboard />}/>
-        <Route path='*' element={<Dashboard />}/>
-      </Routes>
+      <EuiThemeProvider modify={overrides}>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="*" element={<Dashboard />} />
+        </Routes>
+      </EuiThemeProvider>
     </EuiProvider>
-  )
+  );
 }
 
-export default App
+export default App;
