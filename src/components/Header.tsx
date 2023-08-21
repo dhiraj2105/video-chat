33419d04c22c9a214318fea959fafe20c926f120
Dhiraj <dkskp2005@gmail.com>
2023-08-21 12:54:14 +0530

    Theme button and logout button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,29 +2,36 @@ import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate, Link } from "react-router-dom";
 import { useAppSelector } from "../app/hooks";
 import { useDispatch } from "react-redux";
-import { EuiHeader, EuiText, EuiTextColor } from "@elastic/eui";
+import {
+  EuiButtonIcon,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiHeader,
+  EuiText,
+  EuiTextColor,
+} from "@elastic/eui";
+import { firebaseAuth } from "../utils/FirebaseConfig";
+import { signOut } from "firebase/auth";
+import { changeTheme } from "../app/slices/AuthSlice";
 
 function Header() {
   const navigate = useNavigate();
   const location = useLocation();
   const username = useAppSelector((zoom) => zoom.auth.userInfo?.name);
+  const isDarkTheme = useAppSelector((zoom) => zoom.auth.isDarkTheme);
   const [breadCrumbs, setbreadCrumbs] = useState([{ text: "Dashboard" }]);
   const [isResponsive, setisResponsive] = useState(false);
   const dispatch = useDispatch();
+  const logout = () => {
+    signOut(firebaseAuth);
+  };
+  const invertTheme = () => {
+    const theme = localStorage.getItem("zoom-theme");
+    localStorage.setItem("zoom-theme", theme === "light" ? "dark" : "light");
+    dispatch(changeTheme({ isDarkTheme: !isDarkTheme }));
+  };
+
   const section = [
-    {
-      items: [
-        <Link to="/">
-          <EuiText>
-            <h2 style={{ padding: "0 1vw" }}>
-              <EuiTextColor color="#0b5cff">Chat App</EuiTextColor>
-            </h2>
-          </EuiText>
-        </Link>,
-      ],
-    },
-  ];
-  const responsiveSection = [
     {
       items: [
         <Link to="/">
@@ -48,6 +55,60 @@ function Header() {
             </EuiText>
           ) : null}
         </>,
+      ],
+    },
+    {
+      items: [
+        <EuiFlexGroup
+          justifyContent="center"
+          alignItems="center"
+          direction="row"
+          style={{ gap: "2vw" }}
+        >
+          <EuiFlexItem grow={false} style={{ flexBasis: "fit-content" }}>
+            {isDarkTheme ? (
+              <EuiButtonIcon
+                onClick={invertTheme}
+                iconType="sun"
+                color="warning"
+                display="fill"
+                size="s"
+                aria-label="invert-theme-button"
+              />
+            ) : (
+              <EuiButtonIcon
+                onClick={invertTheme}
+                iconType="moon"
+                color="ghost"
+                display="fill"
+                size="s"
+                aria-label="invert-theme-button"
+              />
+            )}
+          </EuiFlexItem>
+          <EuiFlexItem grow={false} style={{ flexBasis: "fit-content" }}>
+            <EuiButtonIcon
+              onClick={logout}
+              iconType="lock"
+              display="fill"
+              size="s"
+              aria-label="logout-button"
+            />
+          </EuiFlexItem>
+        </EuiFlexGroup>,
+      ],
+    },
+  ];
+  const responsiveSection = [
+    {
+      items: [
+        <Link to="/">
+          <EuiText>
+            <h2 style={{ padding: "0 1vw" }}>
+              <EuiTextColor color="#0b5cff">Chat App</EuiTextColor>
+            </h2>
+          </EuiText>
+        </Link>,
       ],
     },
   ];
