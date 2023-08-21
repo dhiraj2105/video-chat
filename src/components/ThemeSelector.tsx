68b1df8bd4efc22cdc25bddcbe1d30b875c201ca
Dhiraj <dkskp2005@gmail.com>
2023-08-21 12:55:52 +0530

    Theme Selection

---
+++ src/components/ThemeSelector.tsx
@@ -0,0 +1,25 @@
+import React, { Suspense, useEffect, useState } from "react";
+import { EuiThemeColorMode } from "@elastic/eui";
+
+const LightTheme = React.lazy(() => import("./Themes/LightTheme"));
+const DarkTheme = React.lazy(() => import("./Themes/DarkTheme"));
+
+function ThemeSelector({ children }: { children: React.ReactNode }) {
+  const [theme, setTheme] = useState<EuiThemeColorMode>("light");
+  useEffect(() => {
+    const theme = localStorage.getItem("zoom-theme");
+    if (theme) {
+      setTheme(theme as EuiThemeColorMode);
+    }
+  });
+  return (
+    <>
+      <Suspense fallback={<></>}>
+        {theme === "dark" ? <DarkTheme /> : <LightTheme />}
+      </Suspense>
+      {children}
+    </>
+  );
+}
+
+export default ThemeSelector;
