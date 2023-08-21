32bdb8ed7915b7307e133b3e83d8195864018553
Dhiraj <dkskp2005@gmail.com>
2023-08-21 12:52:20 +0530

    Themes updation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
-import { EuiProvider, EuiThemeProvider } from "@elastic/eui";
-import React from "react";
+import { EuiProvider, EuiThemeColorMode, EuiThemeProvider } from "@elastic/eui";
+import React, { useEffect, useState } from "react";
 
-import "@elastic/eui/dist/eui_theme_light.css";
+// import "@elastic/eui/dist/eui_theme_light.css";
+// import "@elastic/eui/dist/eui_theme_dark.css";
+
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import { useDispatch } from "react-redux";
+import { useAppSelector } from "./app/hooks";
+import ThemeSelector from "./components/ThemeSelector";
 
 function App() {
+  const dispatch = useDispatch();
+  const isDarkTheme = useAppSelector((zoom) => zoom.auth.isDarkTheme);
+  const [theme, setTheme] = useState<EuiThemeColorMode>("light");
+  const [isInitialTheme, setIsInitialTheme] = useState(true);
+
+  useEffect(() => {
+    const theme = localStorage.getItem("zoom-theme");
+    if (theme) {
+      setTheme(theme as EuiThemeColorMode);
+    } else {
+      localStorage.setItem("zoom-theme", "light");
+    }
+  }, []);
+  useEffect(() => {
+    if (isInitialTheme) setIsInitialTheme(false);
+    else {
+      window.location.reload();
+    }
+  }, [isDarkTheme]);
   //override the defaults
   const overrides = {
     colors: {
@@ -16,15 +40,17 @@ function App() {
   };
 
   return (
-    <EuiProvider>
-      <EuiThemeProvider modify={overrides}>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<Dashboard />} />
-          <Route path="*" element={<Dashboard />} />
-        </Routes>
-      </EuiThemeProvider>
-    </EuiProvider>
+    <ThemeSelector>
+      <EuiProvider colorMode={theme}>
+        <EuiThemeProvider modify={overrides}>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Dashboard />} />
+            <Route path="*" element={<Dashboard />} />
+          </Routes>
+        </EuiThemeProvider>
+      </EuiProvider>
+    </ThemeSelector>
   );
 }
 
