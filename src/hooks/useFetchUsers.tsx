2ef5950b8e7b55cffc2182191b7248f62d156a40
Dhiraj <dkskp2005@gmail.com>
2023-08-23 21:00:47 +0530

    user fetch

--- src/hooks/useFetchUsers.tsx
+++ src/hooks/useFetchUsers.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useAppSelector } from "../app/hooks";
-import { query, where } from "firebase/firestore";
+import { getDocs, query, where } from "firebase/firestore";
 import { userRef } from "../utils/FirebaseConfig";
+import { UserType } from "../utils/Types";
 
 function useFetchUsers() {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState<Array<UserType>>([]);
 
   const uid = useAppSelector((zoom) => zoom.auth.userInfo?.uid);
 
@@ -12,10 +13,21 @@ function useFetchUsers() {
     if (uid) {
       const getUsers = async () => {
         const firestoreQuery = query(userRef, where("uid", "!=", uid));
+        const data = await getDocs(firestoreQuery);
+        const firebaseUsers: Array<UserType> = [];
+        data.forEach((user) => {
+          const userData = user.data() as UserType;
+          firebaseUsers.push({
+            ...userData,
+            label: userData.name,
+          });
+        });
+        setUsers(firebaseUsers);
       };
       getUsers();
     }
   }, [uid]);
+  return [users];
 }
 
 export default useFetchUsers;
