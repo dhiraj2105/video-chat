a20eb56e285c65c9dfcdc77c2cd6c9b83ac7e638
Dhiraj <dkskp2005@gmail.com>
2023-08-27 22:18:16 +0530

    updates

--- src/components/FormComponents/CreateMeetingButtons.tsx
+++ src/components/FormComponents/CreateMeetingButtons.tsx
@@ -4,20 +4,28 @@ import { useNavigate } from "react-router-dom";
 
 function CreateMeetingButtons({
   createmeeting,
+  isEdit,
+  closeFlyout,
 }: {
   createmeeting: () => void;
+  isEdit?: boolean;
+  closeFlyout?: () => {};
 }) {
   const navigate = useNavigate();
   return (
     <EuiFlexGroup>
       <EuiFlexItem grow={false}>
-        <EuiButton color="danger" fill onClick={() => navigate("/")}>
+        <EuiButton
+          color="danger"
+          fill
+          onClick={() => (isEdit ? closeFlyout!() : navigate("/"))}
+        >
           Cancel
         </EuiButton>
       </EuiFlexItem>
       <EuiFlexItem grow={false}>
         <EuiButton fill onClick={createmeeting}>
-          Submit
+          {isEdit ? "Edit Meeting" : "Create Meeting"}
         </EuiButton>
       </EuiFlexItem>
     </EuiFlexGroup>
